4531817d009973099287d0fb8f152babea71ff58
Simone2611 <simone26v@gmail.com>
2024-02-22 17:06:58 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,95 @@
+import bg from "../assets/Free/Background/Blue.png";
+import idle from "../assets/Free/personaggi/rosa/dude.png";
+import ground from "../assets/Free/Terrain/ground.png";
+
+var player;
+var cursors;
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("bg", bg);
+  this.load.image("ground", ground);
+  this.load.spritesheet("idle", idle, {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+import { personaggio } from "./personaggio.js";
+function create() {
+  this.bg = this.add.tileSprite(400, 300, 800, 600, "bg");
+
+  //   platforms = this.physics.add.staticGroup();
+  //   platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  //   platforms.create(600, 400, "ground");
+  //   platforms.create(50, 250, "ground");
+  //   platforms.create(750, 220, "ground");
+
+  player = this.physics.add.sprite(100, 450, "idle");
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "idle", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("idle", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  this.bg.tilePositionX -= 0.2;
+  this.bg.tilePositionY -= 0.1;
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
---
+++ src/personaggio.js
@@ -0,0 +1,4 @@
+import idle from "../assets/Free/personaggi/rosa/idle.png";
+import run from "../assets/Free/personaggi/rosa/run.png";
+
+export function personaggio() {}
