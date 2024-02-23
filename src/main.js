b4c94f9db2ef484b3ce101f34ad55c590974087b
Simone2611 <simone26v@gmail.com>
2024-02-23 11:18:06 +0100

    update v0.2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import bg from "../assets/Free/Background/Blue.png";
 import idle from "../assets/Free/personaggi/rosa/dude.png";
+import dead from "../assets/Free/personaggi/dead.png";
 import ground from "../assets/Free/Terrain/grass.png";
-
+import block1 from "../assets/Free/Terrain/block-1.png";
+import Saw from "../assets/Free/Traps/Saw/sawON.png";
+import spike from "../assets/Free/Traps/Spikes/spike.png";
+import Spikedball from "../assets/Free/Traps/SpikedBall/SpikedBall.png";
 var player;
 var cursors;
 var config = {
@@ -11,7 +15,7 @@ var config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 600 },
       debug: false,
     },
   },
@@ -27,25 +31,50 @@ var game = new Phaser.Game(config);
 function preload() {
   this.load.image("bg", bg);
   this.load.image("ground", ground);
+  this.load.image("block1", block1);
+  this.load.image("spike", spike);
+  this.load.image("Spikedball", Spikedball);
   this.load.spritesheet("idle", idle, {
     frameWidth: 32,
     frameHeight: 48,
   });
+
+  this.load.spritesheet("dead", dead, {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+  this.load.spritesheet("Saw", Saw, {
+    frameWidth: 32,
+    frameHeight: 28,
+  });
 }
-import { personaggio } from "./personaggio.js";
+
 function create() {
+  // Background
+
   this.bg = this.add.tileSprite(400, 300, 800, 600, "bg");
+  // Platform
 
   platforms = this.physics.add.staticGroup();
-
+  //grass
   platforms.create(20, 600, "ground");
 
   for (let i = 50; i < 850; i += 30) {
-    console.log(i);
     platforms.create(i, 600, "ground");
   }
-
-  player = this.physics.add.sprite(100, 450, "idle");
+  // spike
+  spikes = this.physics.add.staticGroup();
+  for (let i = 228; i < 380; i += 15) {
+    spikes.create(i, 577, "spike");
+  }
+  // Spikedball
+  spikes.create(450, 500, "Spikedball");
+  //blocchi
+  platforms.create(200, 560, "block1");
+  platforms.create(382, 560, "block1");
+  platforms.create(520, 560, "block1");
+  //player Creation - Animation
+  player = this.physics.add.sprite(10, 550, "idle");
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
@@ -70,14 +99,38 @@ function create() {
     repeat: -1,
   });
 
-  cursors = this.input.keyboard.createCursorKeys();
+  this.anims.create({
+    key: "dead",
+    frames: this.anims.generateFrameNumbers("dead", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(player, spikes, hitspike, null, this);
+  cursors = this.input.keyboard.createCursorKeys();
+
+  // Saw (da fixare)
+
+  //   this.Saw = this.physics.add.sprite(350, 560, "Saw");
+  //   this.anims.create({
+  //     key: "rotate",
+  //     frames: this.anims.generateFrameNumbers("Saw", { start: 0, end: 7 }),
+  //     frameRate: 10,
+  //     repeat: -1,
+  //   });
+
+  //   this.Saw.play("rotate", this);
+
+  //   this.physics.add.collider(platforms, this.Saw);
+  //   this.physics.add.collider(player, this.Saw);
 }
 
 function update() {
+  // Bg move
   this.bg.tilePositionX -= 0.2;
   this.bg.tilePositionY -= 0.1;
 
+  // Player movement
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
 
@@ -91,8 +144,10 @@ function update() {
 
     player.anims.play("turn");
   }
-
   if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+    player.setVelocityY(-270);
   }
+}
+function hitspike(player, spikes) {
+  this.scene.restart();
 }
---
+++ src/scena1.js
@@ -0,0 +1,22 @@
+// export function personaggio() {
+//   // Background
+
+//   this.bg = this.add.tileSprite(400, 300, 800, 600, "bg");
+//   // Platform
+
+//   platforms = this.physics.add.staticGroup();
+//   //grass
+//   platforms.create(20, 600, "ground");
+
+//   for (let i = 50; i < 850; i += 30) {
+//     platforms.create(i, 600, "ground");
+//   }
+//   // spike
+//   spikes = this.physics.add.staticGroup();
+//   for (let i = 228; i < 380; i += 15) {
+//     spikes.create(i, 577, "spike");
+//   }
+//   //blocchi
+//   platforms.create(200, 560, "block1");
+//   platforms.create(382, 560, "block1");
+// }
