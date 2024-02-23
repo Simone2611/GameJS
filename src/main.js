aa684c18ea1c88c5e3da7677fd2b96551dc66c19
Simone2611 <simone26v@gmail.com>
2024-02-23 12:31:26 +0100

    update v1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import bg from "../assets/Free/Background/Blue.png";
-import idle from "../assets/Free/personaggi/rosa/dude.png";
+import idle from "../assets/Free/personaggi/rosa/idle.png";
+import run from "../assets/Free/personaggi/rosa/run.png";
+import runl from "../assets/Free/personaggi/rosa/runleft.png";
+import fall from "../assets/Free/personaggi/rosa/fall.png";
 import dead from "../assets/Free/personaggi/dead.png";
 import ground from "../assets/Free/Terrain/grass.png";
 import block1 from "../assets/Free/Terrain/block-1.png";
+import slab from "../assets/Free/Terrain/slab.png";
 import Saw from "../assets/Free/Traps/Saw/sawON.png";
 import spike from "../assets/Free/Traps/Spikes/spike.png";
 import Spikedball from "../assets/Free/Traps/SpikedBall/SpikedBall.png";
+import trampolino from "../assets/Free/Traps/Trampoline/trampolino.png";
+import trampolinoIdle from "../assets/Free/Traps/Trampoline/trampidle.png";
 var player;
 var cursors;
 var config = {
@@ -34,17 +40,34 @@ function preload() {
   this.load.image("block1", block1);
   this.load.image("spike", spike);
   this.load.image("Spikedball", Spikedball);
-  this.load.spritesheet("idle", idle, {
+  this.load.image("trampolinoIdle", trampolinoIdle);
+  this.load.image("slab", slab);
+  this.load.spritesheet("idle", run, {
     frameWidth: 32,
-    frameHeight: 48,
+    frameHeight: 32,
   });
-
+  this.load.spritesheet("fall", fall, {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
+  this.load.spritesheet("runl", runl, {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
+  this.load.spritesheet("idletrue", idle, {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
   this.load.spritesheet("dead", dead, {
     frameWidth: 32,
     frameHeight: 48,
   });
   this.load.spritesheet("Saw", Saw, {
     frameWidth: 32,
+    frameHeight: 28,
+  });
+  this.load.spritesheet("fan", trampolino, {
+    frameWidth: 28,
     frameHeight: 28,
   });
 }
@@ -67,48 +90,67 @@ function create() {
   for (let i = 228; i < 380; i += 15) {
     spikes.create(i, 577, "spike");
   }
+
+  for (let i = 400; i < 530; i += 15) {
+    spikes.create(i, 577, "spike");
+  }
+
+  spikes.create(510, 377, "spike");
   // Spikedball
-  spikes.create(450, 500, "Spikedball");
+  spikes.create(455, 520, "Spikedball");
+  spikes.create(620, 395, "Spikedball").setScale(0.85).refreshBody();
+  //slab
+  platforms.create(700, 450, "slab");
+  platforms.create(750, 400, "slab");
+  platforms.create(550, 413, "slab");
   //blocchi
   platforms.create(200, 560, "block1");
   platforms.create(382, 560, "block1");
   platforms.create(520, 560, "block1");
+  platforms.create(520, 400, "block1");
+
   //player Creation - Animation
   player = this.physics.add.sprite(10, 550, "idle");
 
-  player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
   this.anims.create({
     key: "left",
-    frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 3 }),
+    frames: this.anims.generateFrameNumbers("runl", { start: 5, end: 11 }),
     frameRate: 10,
     repeat: -1,
   });
 
   this.anims.create({
-    key: "turn",
-    frames: [{ key: "idle", frame: 4 }],
-    frameRate: 20,
+    key: "fermo",
+    frames: this.anims.generateFrameNumbers("idletrue", { start: 0, end: 10 }),
+    frameRate: 10,
+    repeat: -1,
   });
 
   this.anims.create({
     key: "right",
-    frames: this.anims.generateFrameNumbers("idle", { start: 5, end: 8 }),
+    frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 11 }),
     frameRate: 10,
     repeat: -1,
   });
 
   this.anims.create({
-    key: "dead",
-    frames: this.anims.generateFrameNumbers("dead", { start: 5, end: 8 }),
+    key: "fall",
+    frames: this.anims.generateFrameNumbers("fall", { start: 0, end: 11 }),
     frameRate: 10,
     repeat: -1,
   });
+
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(player, spikes, hitspike, null, this);
   cursors = this.input.keyboard.createCursorKeys();
+  // fan
 
+  this.fan = this.physics.add.staticSprite(600, 567, "fan");
+
+  this.physics.add.collider(platforms, this.fan);
+  this.physics.add.collider(player, this.fan, hittrampolino, null, this);
   // Saw (da fixare)
 
   //   this.Saw = this.physics.add.sprite(350, 560, "Saw");
@@ -139,10 +181,14 @@ function update() {
     player.setVelocityX(160);
 
     player.anims.play("right", true);
+  } else if (!player.body.touching.down) {
+    player.setVelocityX(0);
+
+    player.anims.play("fall", true);
   } else {
     player.setVelocityX(0);
 
-    player.anims.play("turn");
+    player.anims.play("fermo", true);
   }
   if (cursors.up.isDown && player.body.touching.down) {
     player.setVelocityY(-270);
@@ -150,4 +196,16 @@ function update() {
 }
 function hitspike(player, spikes) {
   this.scene.restart();
+}
+
+function hittrampolino(player, fan) {
+  this.anims.create({
+    key: "trampolino",
+    frames: this.anims.generateFrameNumbers("fan", { start: 0, end: 7 }),
+    frameRate: 10,
+    repeat: 0,
+  });
+
+  this.fan.play("trampolino", this);
+  player.setVelocityY(-400);
 }
