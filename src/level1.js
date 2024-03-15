5ddee5048a53eaf90cc31dc7b842eb5e31a864b2
Simone2611 <simone26v@gmail.com>
2024-03-15 17:22:29 +0100

    fix

--- src/level1.js
+++ src/level1.js
@@ -49,6 +49,7 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  parent: "game",
   physics: {
     default: "arcade",
     arcade: {
--- src/main.js
+++ src/main.js
@@ -47,6 +47,7 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  parent: "game",
   physics: {
     default: "arcade",
     arcade: {
